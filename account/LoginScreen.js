623294ac496ec36d8b676f2ac45b715acee0bced
Netta <n2ojne00@students.oamk.fi>
2025-07-24 16:12:54 +0300

    sign up and login with firebase email/password

--- account/LoginScreen.js
+++ account/LoginScreen.js
@@ -1,23 +1,58 @@
 import { View, Text } from 'react-native';
+import { useState } from 'react';
+
 import base from '../styles/base';
 import { InputText } from '../components/txtInput';
 import { CustomButton } from '../components/pressable';
 import txtStyles from '../styles/text';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { AUTH } from '../FirebaseConfig';
 
 export default function LoginScreen() {
 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        try {
+            const userCredential = await signInWithEmailAndPassword(AUTH, email, password);
+            const user = userCredential.user;
+            alert('Login successful!');
+            console.log('Logged in:', user);
+            // Voit ohjata käyttäjän etusivulle tms.
+        } catch (error) {
+            console.error('Login error:', error.message);
+        }
+    };
+
     return (
         <View style={base.container}>
-            
+
             <View style={base.loginBox}>
                 <Text style={txtStyles.title}>LOGIN</Text>
-                {//validator for username needed
-                }
 
-                <InputText title='Email' placeholder='email' keytype='email-address' contentType='email' validationType='email' />
-                <InputText title='Password' placeholder='password' keytype='default' contentType='password' secure={true} validationType='password' />
+                <InputText
+                    title='Email'
+                    placeholder='email'
+                    keytype='email-address'
+                    contentType='email'
+                    validationType='email'
+                    value={email}
+                    onChangeText={setEmail}
+                />
 
-                <CustomButton title="Login" onPress={() => { }} />
+                <InputText
+                    title='Password'
+                    placeholder='password'
+                    keytype='default'
+                    contentType='password'
+                    secure={true}
+                    //validationType='password'
+                    value={password}
+                    onChangeText={setPassword}
+                />
+
+                <CustomButton title="Login" onPress={handleLogin} />
             </View>
         </View>
     );
--- account/SignUpScreen.js
+++ account/SignUpScreen.js
@@ -1,24 +1,67 @@
 import { View, Text } from 'react-native';
+import { useState } from 'react';
+
 import base from '../styles/base';
 import { InputText } from '../components/txtInput';
 import { CustomButton } from '../components/pressable';
 import txtStyles from '../styles/text';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { AUTH } from '../FirebaseConfig';
+
 
 export default function SignUpScreen() {
 
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+      const signUp = async () => {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(AUTH, email, password);
+      const user = userCredential.user;
+      console.log("User registered:", user);
+    } catch (error) {
+      console.error("Registration error:", error.message);
+    }
+  };
+
+
     return (
         <View style={base.container}>
-            
+
             <View style={base.loginBox}>
                 <Text style={txtStyles.title}>SIGN UP</Text>
+                
                 {//validator for username needed
                 }
+                <InputText title='Username'
+                    placeholder='username'
+                    keytype='text'
+                    validationType='username'
+                    value={username}
+                    onChangeText={setUsername}
+                />
 
-                <InputText title='Username' placeholder='username' keytype='text' />
-                <InputText title='Email' placeholder='email' keytype='email-address' contentType='email' validationType='email' />
-                <InputText title='Password' placeholder='password' keytype='default' contentType='password' secure={true} validationType='password' />
+                <InputText title='Email'
+                    placeholder='email'
+                    keytype='email-address'
+                    contentType='email'
+                    validationType='email'
+                    value={email}
+                    onChangeText={setEmail}
+                />
 
-                <CustomButton title="Sign Up" onPress={() => { }} />
+                <InputText title='Password'
+                    placeholder='password'
+                    keytype='default'
+                    contentType='password'
+                    secure={true}
+                    validationType='password'
+                    value={password}
+                    onChangeText={setPassword}
+                />
+
+                <CustomButton title="Sign Up" onPress={signUp} />
             </View>
         </View>
     );
